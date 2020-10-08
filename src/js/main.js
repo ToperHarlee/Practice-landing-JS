c1ee36ee13dcdc0a8405a9324640acdf29e1a88a
Mikhail <epictapok@gmail.com>
2020-10-08 10:22:33 +0300

    написаны табы

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,14 @@
 
 import './slider';
 import modals from "./modules/modals";
+import tabs from "./modules/tabs";
 
-
+//лучше без обработчика тк при загрузке страницы функция будет срабатывать (например модальные окна) а потом через таймаут еще раз
 window.addEventListener('DOMContentLoaded', () => {
 
     modals();
-
-
-
-
-
-
-
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 
 
 });
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -43,6 +43,7 @@ const modals = () => {
         setTimeout(() => {
             openModal(document.querySelector(selector));
         }, time);
+        clearTimeout();
     }
     
     /*const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
@@ -51,7 +52,7 @@ const modals = () => {
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
-    showModalByTime('.popup', 3000)
+    showModalByTime('.popup', 60000);
 };
 
 export default modals;
